9ea829cf51f5c3629687e05e81a271b0c733b651
Lming1 <spear131121@gmail.com>
2016-05-14 18:34:13 +0900

    test2

--- test/test.cs
+++ test/test.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp7
 {
     class GlazerCalc
     {
@@ -16,40 +16,24 @@ namespace ConsoleApp5
             const double MAX_HEIGHT = 3.0;
             const double MIN_HEIGHT = 0.75;
             string widthString, heightString;
-            Console.Write("Give the width of the window :");
-            widthString = Console.ReadLine();
-            width = double.Parse(widthString);
-                if (width < MIN_WIDTH)
-                {
-                    Console.WriteLine("Width is too small.\n\n");
-                    Console.WriteLine("Using minimum");
-                    width = MIN_WIDTH;
-                }
-                if(width>MAX_WIDTH)
-                {
-                    Console.WriteLine("Width is too large.\n\n");
-                    Console.WriteLine("Using maximum");
-                    width = MAX_WIDTH;
-                }
-            Console.Write("Give the height of the window :");
-            heightString = Console.ReadLine();
-            height = double.Parse(heightString);
-                if(height<MIN_HEIGHT)
-                {
-                    Console.WriteLine("Height is too small.\n\n");
-                    Console.WriteLine("Using minimum");
-                    height = MIN_HEIGHT;
-                }
-                if(height>MAX_HEIGHT)
-                {
-                    Console.WriteLine("Height is too large.\n\n");
-                    Console.WriteLine("Using maximum");
-                    height = MAX_HEIGHT;
-                }   
+            do
+            {
+                Console.Write("Give the width of the window between " + MIN_WIDTH + " and " + MAX_WIDTH + " :");
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
+            }
+            while (width < MIN_WIDTH || width>MAX_WIDTH);
+            do
+            {
+                Console.Write("Give the height of the window between " + MIN_HEIGHT + " and " + MAX_HEIGHT + " :");
+                heightString = Console.ReadLine();
+                height = double.Parse(heightString);
+            }
+            while (height < MIN_HEIGHT || height > MAX_HEIGHT);
             woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
             Console.WriteLine("The length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");        
         }
     }
 }
