9864b89b93dae157d0791cb244cd90f55a1ef0cb
Lming1 <spear131121@gmail.com>
2016-05-15 23:36:33 +0900

    study 0515

---
+++ study/Ball/Assets/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    int count = 1;
+
+    float startingPoint;
+
+	// Use this for initialization
+	void Start () {
+
+
+        Debug.Log("Start");
+        startingPoint = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+        float distance;
+        distance = transform.position.z - startingPoint;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*300);
+        }
+	}
+  
+}
---
+++ study/Ball/Assets/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+    float delta = -0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        float newXposition = transform.localPosition.x + delta;
+        transform.localPosition = new Vector3(newXposition,transform.localPosition.y,
+            transform.localPosition.z);
+        if (transform.localPosition.x < -3.5)
+        {
+            delta = 0.1f;
+        }
+        else if (transform.localPosition.x > 3.5)
+        {
+            delta = -0.1f;
+        }
+    }
+}
