4d993015915e978302feaa83f73bd186dfdfc7ad
Lming1 <spear131121@gmail.com>
2016-05-20 01:08:59 +0900

    second

--- study/CameraWork.cs
+++ study/CameraWork.cs
@@ -7,6 +7,13 @@ public class CameraWork : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         ball = GameObject.Find("Ball");
+        GameObject[] coins= GameObject.FindGameObjectsWithTag("Coin");     
+        for(int i = 0; i < coins.Length; i++)
+        {
+            Debug.Log(coins[i].name);
+        }
+
+        
 	
 	}
 	
--- study/Obstacle.cs
+++ study/Obstacle.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class Obstacle : MonoBehaviour {
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = transform.position - collision.gameObject.transform.position;
+        direction = direction.normalized * 1000;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(direction);
+    }
     float delta = -0.1f;
 	// Use this for initialization
 	void Start () {
-	
+     
 	}
 	
 	// Update is called once per frame
-	void Update () {        
+	void Update () {
+     
         float newXposition = transform.localPosition.x + delta;
         transform.localPosition = new Vector3(newXposition,transform.localPosition.y,
             transform.localPosition.z);
